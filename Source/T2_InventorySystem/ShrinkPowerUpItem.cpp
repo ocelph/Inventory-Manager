e3fc7c471891666fc9132bea25481e9db8d69cd3
Oceane Leung-Pah-Hang <oleu0002@student.monash.edu>
2026-03-18 22:30:16 +1100

    Removed comments and shrunk character using SetRelativeScale3D so that it shrinks both mesh and collision component

--- Source/T2_InventorySystem/ShrinkPowerUpItem.cpp
+++ Source/T2_InventorySystem/ShrinkPowerUpItem.cpp
@@ -17,37 +17,14 @@ void UShrinkPowerUpItem::UseItem(AT2_InventorySystemCharacter* Character)
 {
 	Super::UseItem(Character);
 	
-	UE_LOG(LogTemp, Warning, TEXT("Entered ShrinkPowerUpItem"));
-	UE_LOG(LogTemp, Warning, TEXT("shrinkPowerUpActivate = %s"), Character->shrinkPowerUpActivate ? TEXT("true") : TEXT("false"));
-	
 	if (Character)
 	{
 		if (!(Character->shrinkPowerUpActivate))
 		{
-			
-			
 			AffectedCharacter = Character;
 			AffectedCharacter->shrinkPowerUpActivate = true;
-			
-			
-			AffectedCharacter->GetMesh()->SetWorldScale3D(FVector(ShrinkPowerUpPercentage));
-			// UCapsuleComponent* CollisonCapsule = AffectedCharacter->GetCapsuleComponent();
-			//
-			// // Half Height = distance from the center of the capsule to the top (or bottom)
-			// float InitialHalfHeight = CollisonCapsule->GetUnscaledCapsuleHalfHeight();
-			// float InitialRadius = CollisonCapsule->GetUnscaledCapsuleRadius();
-			//
-			// UE_LOG(LogTemp, Warning, TEXT("InitialHalfHeight: %f"),InitialHalfHeight );
-			// UE_LOG(LogTemp, Warning, TEXT("InitialRadius: %f"),InitialRadius );
-			//
-			// CollisonCapsule->SetCapsuleHalfHeight(InitialHalfHeight * ShrinkPowerUpPercentage);
-			// CollisonCapsule->SetCapsuleRadius(InitialRadius * ShrinkPowerUpPercentage);
-			
-			// UE_LOG(LogTemp, Warning, TEXT("CurrentHalfHeight: %f"), CollisonCapsule->GetUnscaledCapsuleHalfHeight() );
-			// UE_LOG(LogTemp, Warning, TEXT("CurrentlRadius: %f"),CollisonCapsule->GetUnscaledCapsuleRadius() );
-			
+			AffectedCharacter->GetCapsuleComponent()->SetRelativeScale3D(FVector(ShrinkPowerUpPercentage, ShrinkPowerUpPercentage, ShrinkPowerUpPercentage));
 			Character->GetWorld()->GetTimerManager().SetTimer(ShrinkTimer, this, &UShrinkPowerUpItem::OnShrinkPowerUpEnd, ShrinkPowerUpDuration, false);
-		
 			
 		}
 		
@@ -60,14 +37,7 @@ void UShrinkPowerUpItem::OnShrinkPowerUpEnd()
 	{
 		if(AffectedCharacter) {
 			
-			AffectedCharacter->GetMesh()->SetWorldScale3D(FVector(1));
-			
-			// float CurrentHalfHeight = AffectedCharacter->GetCapsuleComponent()->GetUnscaledCapsuleHalfHeight();
-			// float CurrentRadius = AffectedCharacter->GetCapsuleComponent()->GetUnscaledCapsuleRadius();
-			//
-			// AffectedCharacter->GetCapsuleComponent()->SetCapsuleHalfHeight(CurrentHalfHeight * (1/ShrinkPowerUpPercentage));
-			// AffectedCharacter->GetCapsuleComponent()->SetCapsuleRadius(CurrentRadius * (1/ShrinkPowerUpPercentage));
-			
+			AffectedCharacter->GetCapsuleComponent()->SetRelativeScale3D(FVector(1, 1, 1));
 			AffectedCharacter->shrinkPowerUpActivate = false;
 			AffectedCharacter = nullptr;
 		}
